3ce77a384cf8dba2d4dc7671e95d6fd319546b57
Frank <frank@Franks-MacBook-Pro-2.local>
2018-08-22 14:13:43 +0800

    Add asList data and mutation function to store

--- src/store/module-example/mutations.js
+++ src/store/module-example/mutations.js
@@ -1,4 +1,19 @@
 /*
-export function someMutation (state) {
+export const configName = (state, payload) => {
+  state.asList.filter(as => as.number === payload.asNumber)[0].name = payload.newName;
 }
 */
+
+// NOTE: Readability!!!
+export const configName = (state, payload) => {
+  let asList = state.asList
+  let asNumber = payload.asNumber
+  let newName = payload.newName
+
+  // Find the as that matches the as number in asList
+  let as = asList.find(as => as.number === asNumber)
+
+  if (as) {
+    as.name = newName
+  }
+}
--- src/store/module-example/state.js
+++ src/store/module-example/state.js
@@ -1,3 +1,13 @@
 export default {
-  //
+  asList: [
+    { number: '100', name: 'TW' },
+    { number: '200', name: 'JP' },
+    { number: '300', name: 'CH' },
+    { number: '400', name: '' },
+    { number: '500', name: '' },
+    { number: '600', name: '' },
+    { number: '700', name: '' },
+    { number: '800', name: '' },
+    { number: '900', name: '' }
+  ]
 }
