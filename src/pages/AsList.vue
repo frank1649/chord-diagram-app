ea3e0c4236491c950b68d214f596253b478ddb38
Frank <frank@Franks-MacBook-Pro-2.local>
2018-08-22 14:11:28 +0800

    Add About and AsList page

---
+++ src/pages/AsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="q-ma-xl">
+    <p>AS List</p>
+    <as-block
+      v-for="as in asList"
+      :as="as"
+      :key="as.number"
+    />
+  </div>
+</template>
+
+<style lang="stylus">
+</style>
+
+<script>
+import AsBlock from '../components/AsBlock.vue'
+
+export default {
+  name: 'AsList',
+  data () {
+    return {
+      asList: this.$store.state.example.asList
+    }
+  },
+  components: {
+    AsBlock
+  }
+}
+</script>
